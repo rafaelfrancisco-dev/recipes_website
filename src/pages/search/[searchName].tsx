0970a18da45dbf7406e7cdf5deba815cace9d847
Rafael Francisco <rafaelfrancisco6@gmail.com>
2021-04-08 19:05:51 +0100

    Search working

--- src/pages/search/[searchName].tsx
+++ src/pages/search/[searchName].tsx
@@ -1,42 +1,54 @@
 import React from 'react';
 
-import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from 'next';
 
 import { Meta } from '../../layout/Meta';
 import { Recipe } from '../../models/Recipe';
 import { Main } from '../../templates/Main';
+import Link from 'next/link';
 
-const recipes = require('../../models/recipes.json');
+function searchRecipes(name: string | string[]): Array<Recipe> {
+  const recipes = require('../../models/recipes.json');
 
-function searchRecipes(name: string | string[]): Array<Recipe> | null {
   const searchName = typeof name === 'string' ? name.toLowerCase() : name[0].toLowerCase();
   const foundArray = recipes.filter((e: Recipe) => e.strMeal.toLowerCase().includes(searchName));
 
   if (foundArray !== undefined) {
     return foundArray;
+  } else {
+    return [];
   }
-  return [];
 }
 
-export const getServerSideProps: GetServerSideProps = async ({
-  params,
-}: GetServerSidePropsContext) => ({
+export const getServerSideProps: GetServerSideProps = async ({ params }: GetServerSidePropsContext) => ({
   props: {
     recipes: searchRecipes(params?.searchName === undefined ? '' : params.searchName),
-  },
+    searchedText: params?.searchName
+  }
 });
 
-const SearchPage = ({ props }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
+const SearchPage: NextPage = ({ recipes, searchedText }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
   <Main
     meta={(
       <Meta
-        title="Next.js Boilerplate Presentation"
-        description="Next js Boilerplate is the perfect starer code for your project. Build your React application with Next.js framework."
+        title='Next.js Boilerplate Presentation'
+        description='Next js Boilerplate is the perfect starer code for your project. Build your React application with Next.js framework.'
       />
     )}
   >
     <div>
-      <h2>{props.recipes.length}</h2>
+      <p>Search for {searchedText}</p>
+      <p>Found {recipes.length} results</p>
+
+      <br />
+
+      {recipes.map((r: Recipe, index: number) => (
+        <div key={index}>
+          <Link key={r.idMeal} href={`/recipes/${r.idMeal}`}>
+            <p>{r.strMeal}</p>
+          </Link>
+        </div>
+      ))}
     </div>
   </Main>
 );
