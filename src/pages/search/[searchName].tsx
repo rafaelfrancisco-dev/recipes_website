a9227767b5602c58840755a35a3b822839a6bbd6
Rafael Francisco <rafaelfrancisco6@gmail.com>
2021-04-08 17:24:52 +0100

    Finish server side logic for search

--- src/pages/search/[searchName].tsx
+++ src/pages/search/[searchName].tsx
@@ -1,36 +1,44 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import React from 'react';
+
+import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+
+import { Meta } from '../../layout/Meta';
 import { Recipe } from '../../models/Recipe';
+import { Main } from '../../templates/Main';
 
 const recipes = require('../../models/recipes.json');
 
-function searchRecipes(name: string): Array<Recipe> | null {
-  const foundArray = recipes.filter((e: Recipe) => e.strMeal.includes(name));
-  if (foundArray.length > 0) {
+function searchRecipes(name: string | string[]): Array<Recipe> | null {
+  const searchName = typeof name === 'string' ? name.toLowerCase() : name[0].toLowerCase();
+  const foundArray = recipes.filter((e: Recipe) => e.strMeal.toLowerCase().includes(searchName));
+
+  if (foundArray !== undefined) {
     return foundArray;
-  } else {
-    return null;
   }
+  return [];
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  if (context.params?.searchName instanceof String) {
-    return {
-      props: {
-        recipes: searchRecipes(context.params.searchName as string),
-      }
-    }
-  } else {
-    return {
-      notFound: true,
-    }
-  } 
-}
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext) => ({
+  props: {
+    recipes: searchRecipes(params?.searchName === undefined ? '' : params.searchName),
+  },
+});
 
-function SearchPage({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  <div>
-    <h1>Search page</h1>
-    <h2>{data.recipes.length}</h2>
-  </div>
-}
+const SearchPage = ({ props }: InferGetServerSidePropsType<typeof getServerSideProps>) => (
+  <Main
+    meta={(
+      <Meta
+        title="Next.js Boilerplate Presentation"
+        description="Next js Boilerplate is the perfect starer code for your project. Build your React application with Next.js framework."
+      />
+    )}
+  >
+    <div>
+      <h2>{props.recipes.length}</h2>
+    </div>
+  </Main>
+);
 
 export default SearchPage;
